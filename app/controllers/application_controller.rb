71e0bea2bb38f3fc833233f7a08382b0fa515a3b
chenyunli <m18301662790@gmail.com>
2018-04-04 16:21:24 +0800

    add posts to root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate_admin!
-    redirect_to root_path, alert: "没有权限浏览该页面" if !current_user.is_admin
+    redirect_to root_path, alert: "没有权限浏览该页面" if !is_admin?
+  end
+
+  helper_method :is_admin?
+
+  def is_admin?
+    current_user && current_user.is_admin
   end
 end
